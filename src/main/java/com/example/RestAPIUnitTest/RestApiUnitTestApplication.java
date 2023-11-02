153fb3376ea783a68f7e977d3b9b864af977d663
BikashGhale773 <ghalebikash445@gmail.com>
2023-11-01 23:51:49 -0500

    New application created

---
+++ src/main/java/com/example/RestAPIUnitTest/RestApiUnitTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.RestAPIUnitTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiUnitTestApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}

4241a88df3beb17f0feb9714d3f7b45d2ea279dc
BikashGhale773 <ghalebikash445@gmail.com>
2023-11-03 20:25:00 -0500

    Created Calculator class with two function

---
+++ src/test/java/com/example/RestAPIUnitTest/Calculator.java
@@ -0,0 +1,15 @@
+package com.example.RestAPIUnitTest;
+
+public class Calculator {
+    public int sum(int a,int b){
+        return a + b;
+    }
+
+    public int product(int a, int b){
+        return a * b;
+    }
+
+    public Boolean compareNum(int a, int b){
+        return a == b;
+    }
+}
